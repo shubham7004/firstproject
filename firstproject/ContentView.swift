87d605898d03ab6bc06f0f44a3e7326f9f7d58da
shubham7004 <shubham7004>
2021-09-17 14:06:13 +0530

    Initial Commit

---
+++ firstproject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firstproject
+//
+//  Created by Smishra2 on 17/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ firstproject/firstprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstprojectApp.swift
+//  firstproject
+//
+//  Created by Smishra2 on 17/09/21.
+//
+
+import SwiftUI
+
+@main
+struct firstprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
