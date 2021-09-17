73039e8afe4769309586257c60763cbd4b9514b9
shubham7004 <shubham7004>
2021-09-17 14:33:59 +0530

    this is 2nd commite

--- firstproject/ContentView.swift
+++ firstproject/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, world! i am goint to do 2nd commite")
             .padding()
     }
 }
